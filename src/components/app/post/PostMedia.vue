5c49733cd190e8a5d275a5fd386c804e0ebe8c6e
Adedero <118962395+Adedero@users.noreply.github.com>
2024-06-12 11:15:05 +0100

    Fixed images attached to a post

--- src/components/app/post/PostMedia.vue
+++ src/components/app/post/PostMedia.vue
@@ -7,16 +7,25 @@ defineProps({
 </script>
 
 <template>
-  <Carousel :value="media" :numVisible="1" :numScroll="1">
-    <template #item="slotProps">
-      <div class="grid place-content-center">
-        <Image v-if="slotProps.data.type.includes('image')" :src="slotProps.data.url" alt="Image" width="100%"
-          preview />
-        <video v-if="slotProps.data.type.includes('video')" controls>
-          <source :src="slotProps.data.url" :type="slotProps.data.type">
-        </video>
-      </div>
-    </template>
-  </Carousel>
-
+  <div v-if="media.length < 2">
+    <div class="grid place-content-center">
+      <Image v-if="media[0].type.includes('image')" :src="media[0].url" alt="Image" width="100%" preview />
+      <video v-if="media[0].type.includes('video')" controls class="w-full">
+        <source :src="media[0].url" :type="media[0].type">
+      </video>
+    </div>
+  </div>
+  <div v-else>
+    <Carousel :value="media" :numVisible="1" :numScroll="1">
+      <template #item="slotProps">
+        <div class="grid place-content-center">
+          <Image v-if="slotProps.data.type.includes('image')" :src="slotProps.data.url" alt="Image" width="100%"
+            preview />
+          <video v-if="slotProps.data.type.includes('video')" controls>
+            <source :src="slotProps.data.url" :type="slotProps.data.type">
+          </video>
+        </div>
+      </template>
+    </Carousel>
+  </div>
 </template>
