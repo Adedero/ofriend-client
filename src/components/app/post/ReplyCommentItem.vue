6fbf5637854450ff2ca7e96017f898a5e4941c55
Adedero <118962395+Adedero@users.noreply.github.com>
2024-06-14 22:42:45 +0100

    Update ReplyCommentItem.vue

--- src/components/app/post/ReplyCommentItem.vue
+++ src/components/app/post/ReplyCommentItem.vue
@@ -14,7 +14,7 @@ defineProps({
 <template>
   <div>
     <div class="flex gap-1">
-      <DynamicAvatar shape="circle" :user="comment.author" />
+      <DynamicAvatar shape="circle" :user="comment.author" class="h-12 w-12" />
       <div class="w-full">
         <div class="flex items-center justify-between">
           <div class="flex items-center gap-2">
