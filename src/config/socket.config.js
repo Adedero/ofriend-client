26510ef7faaeba5db55ef7d47c10e425e6932039
Adedero <118962395+Adedero@users.noreply.github.com>
2024-06-23 09:51:19 +0100

    Update socket.config.js

--- src/config/socket.config.js
+++ src/config/socket.config.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_API.slice(0, -1);
+//const URL = import.meta.env.VITE_API
+const URL = "https://ofriend-server.onrender.com";
 const socket = io(URL, {
     autoConnect: false, // we will manually connect in Vue components
 });
