bccaf7889d6506e02360ac79a9c709839bd8f013
Adedero <118962395+Adedero@users.noreply.github.com>
2024-06-27 16:51:34 +0100

    fixed bugs

--- src/config/socket.config.js
+++ src/config/socket.config.js
@@ -3,7 +3,6 @@ import { io } from 'socket.io-client';
 const URL = import.meta.env.VITE_SERVER_URL;
 const socket = io(URL, {
     autoConnect: false,
-    closeOnBeforeunload: true,
 });
 
 socket.on('connect_error', () => {
