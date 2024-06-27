5bab1c0d43e4d35a82243302f38f1fe1bfa6093f
Adedero <118962395+Adedero@users.noreply.github.com>
2024-06-27 14:58:20 +0100

    fixed bugs

--- src/config/socket.config.js
+++ src/config/socket.config.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_API;
+const URL = import.meta.env.SERVER_URL;
 const socket = io(URL, {
     autoConnect: false, // we will manually connect in Vue components
 });
