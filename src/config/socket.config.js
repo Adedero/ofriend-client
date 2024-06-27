08ed6b1deecad398c5912336663287218bb12ecd
Adedero <118962395+Adedero@users.noreply.github.com>
2024-06-27 17:04:48 +0100

    socket bug

--- src/config/socket.config.js
+++ src/config/socket.config.js
@@ -3,6 +3,7 @@ import { io } from 'socket.io-client';
 const URL = import.meta.env.VITE_SERVER_URL;
 const socket = io(URL, {
     autoConnect: false,
+    transports: ['websocket']
 });
 
 socket.on('connect_error', (error) => {
