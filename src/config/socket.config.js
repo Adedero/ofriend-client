27e444aef8533c2601c7dcd4900e8bb37bdc78df
Adedero <118962395+Adedero@users.noreply.github.com>
2024-06-27 15:21:33 +0100

    Fixed bugs

--- src/config/socket.config.js
+++ src/config/socket.config.js
@@ -2,7 +2,12 @@ import { io } from 'socket.io-client';
 
 const URL = import.meta.env.VITE_SERVER_URL;
 const socket = io(URL, {
-    autoConnect: false, // we will manually connect in Vue components
+    autoConnect: false,
+    closeOnBeforeunload: true,
 });
+
+socket.on('error', (error) => {
+    console.log('Error establishing socket connection: ', error)
+})
 
 export default socket;
