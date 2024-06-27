3f4d808df45ebba94088e2ea214269ab02936e76
Adedero <118962395+Adedero@users.noreply.github.com>
2024-06-27 21:19:45 +0100

    Fixed socket bugs

--- src/config/socket.config.js
+++ src/config/socket.config.js
@@ -3,11 +3,10 @@ import { io } from 'socket.io-client';
 const URL = import.meta.env.VITE_SERVER_URL;
 const socket = io(URL, {
     autoConnect: false,
-    transports: ['websocket']
 });
 
 socket.on('connect_error', (error) => {
-    console.log('Error establishing socket connection: ', error);
+    console.log('Error establishing socket connection: ', error, error.message);
     setTimeout(() => {
     socket.connect();
   }, 1000);
